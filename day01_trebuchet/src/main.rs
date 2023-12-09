aa195a34086fc4749c3beec1b4f7311824205b6b
Michael Jagroep <michael.jagroep@gmail.com>
2023-12-09 16:16:06 +0000

    day 1 solution

--- day01_trebuchet/src/main.rs
+++ day01_trebuchet/src/main.rs
@@ -1,6 +1,12 @@
-use std::{fs::File, env::{Args, self}, io::{BufReader, BufRead}};
+use std::{fs::File, env::{Args, self}, io::{BufReader, BufRead, Error}};
 
 const ARGS_IX_INPUT : usize = 1;
+
+macro_rules! debug {
+    ($a:expr) => {
+        println!("{}",$a)
+    };
+}
 
 fn main()  {
     let input_file_path = env::args().nth(ARGS_IX_INPUT);
@@ -19,9 +25,57 @@ fn main()  {
     let input_file = input_file.unwrap();
     let input_file = BufReader::new(input_file);
 
-    
+    let mut sum : usize = 0; 
     for line in input_file.lines() {
-
-        println!("{}",line.unwrap());
+        
+        sum = sum + get_line_value(line.unwrap());
     }
+
+    println!("The answer is {}", sum);
 }
+
+fn get_line_value(line : String) -> usize {
+   
+    let mut first_digit_char : char = '\0';
+    let mut last_digit_char : char = '\0';
+
+    debug!(line);
+
+
+    for c in line.chars() {
+
+        if !c.is_numeric() {
+            continue;
+        }
+        
+        if first_digit_char == '\0' {
+           first_digit_char = c;
+        } else {
+            last_digit_char = c;
+        }
+        
+    }
+    if first_digit_char == '\0' {
+        return 0;
+    } 
+    let mut digits_text = String::from("");
+    digits_text.push(first_digit_char);
+    if last_digit_char != '\0' {
+        digits_text.push(last_digit_char);
+    } else {
+        digits_text.push(first_digit_char);
+    }
+    debug!(digits_text);
+
+    let result = match digits_text.parse::<usize>() {
+        Ok(value) => value,
+        Err(_) => 0
+    };
+
+    debug!(result);
+    
+    return result;
+    
+}
+
+
