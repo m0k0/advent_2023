c06782c96f8c778b131fb40cd8b1d43406fc045b
Michael Jagroep <michael.jagroep@gmail.com>
2023-12-09 15:06:22 +0000

    read input file

--- day01_trebuchet/src/main.rs
+++ day01_trebuchet/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, env::{Args, self}, io::{BufReader, BufRead}};
+
+const ARGS_IX_INPUT : usize = 1;
+
+fn main()  {
+    let input_file_path = env::args().nth(ARGS_IX_INPUT);
+
+    if input_file_path.is_none() { 
+        panic!("No input file specified");
+    }
+    let input_file_path = input_file_path.unwrap();
+
+
+    let input_file = File::open(&input_file_path);
+    if input_file.is_err() {
+        panic!("Error opening file: {}", &input_file_path)
+    }
+
+    let input_file = input_file.unwrap();
+    let input_file = BufReader::new(input_file);
+
+    
+    for line in input_file.lines() {
+
+        println!("{}",line.unwrap());
+    }
 }
