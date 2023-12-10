7c3c4eafdfdbab0a1c1a1e296adf24699d8575d0
Michael Jagroep <michael.jagroep@gmail.com>
2023-12-10 17:57:18 +0000

    fixed soln; complete processing buffered symbols/parts

--- day03_gear-ratios/src/main.rs
+++ day03_gear-ratios/src/main.rs
@@ -23,107 +23,117 @@ fn find_parts_adjacent_to_symbols(
     parts_store: &Vec<PartNumber>) 
     -> Vec<usize> {
 
-    let mut result: Vec<usize> = Vec::new();
+        let mut result: Vec<usize> = Vec::new();
 
-    for symbol in symbols {
-        debug!("symbol: {}", symbol.value); 
+        for symbol in symbols {
+            debug!("symbol: {}", symbol.value); 
 
-        for part_id in part_ids_in_range {
+            for part_id in part_ids_in_range {
 
-            let ref part = parts_store[*part_id];
-            if is_part_adjacent(&part, symbol.index) {
-                result.push(*part_id);
+                let ref part = parts_store[*part_id];
+                if is_part_adjacent(&part, symbol.index) {
+                    result.push(*part_id);
+                }
             }
         }
-    }
 
-    return result;
-}
+        return result;
+    }
 
 fn is_part_adjacent(
     part: &PartNumber,
     index: usize) 
     -> bool{
-    
-    let mut start = 0;
-    if part.index > 0 {
-        start = part.index - 1;
+
+        let mut start = 0;
+        if part.index > 0 {
+            start = part.index - 1;
+        }
+        let end = part.index + part.length; 
+
+        if index >= start && index <= end {
+
+            debug!("\t   - adjacent: {}", part.value);
+
+            return true;
+        }
+
+        debug!("\t ! - not adjacent: {}", part.value );
+        /*
+           debug!("\t\t ix: {}", index);
+           debug!("\t\t pix: {}", part.index);
+           debug!("\t\t s: {}", start);
+           debug!("\t\t e: {}", end);
+           */
+        return false;
     }
-    let end = part.index + part.length; 
 
-    if index >= start && index <= end {
+fn get_adjacent_part_ids(  
+    symbols_in_range: &Vec<Symbol>,
+    part_radar: &VecDeque<Vec<usize>>,
+    parts_store: &Vec<PartNumber>
+    )-> Vec<usize> {
 
-        debug!("\t   - adjacent: {}", part.value);
+    let mut part_ids_in_range: Vec<usize> = Vec::new();
+    for i in 0..(RADAR_LINE_RANGE*2)+1 {
 
-        return true;
+        if let Some(parts_on_line) = part_radar.get(i) {
+            //debug!("part_radar index: {}", i);
+            for part_id in parts_on_line {
+
+                //debug!("- part in range: {}", parts_store[*part_id].value);
+                part_ids_in_range.push(*part_id);
+            }
+        }
     }
-    
-    debug!("\t ! - not adjacent: {}", part.value );
-    /*
-    debug!("\t\t ix: {}", index);
-    debug!("\t\t pix: {}", part.index);
-    debug!("\t\t s: {}", start);
-    debug!("\t\t e: {}", end);
-    */
-    return false;
+
+
+    let adjacent_part_ids = find_parts_adjacent_to_symbols(
+        symbols_in_range, &part_ids_in_range, parts_store);
+
+    return adjacent_part_ids; 
+
 }
+const PADDING_CHAR: char = '.';
+const RADAR_LINE_RANGE: usize = 1;
 
 
 fn main() {
-   
+
     env::set_var("PRINT_DEBUG", "true");
 
-    const PADDING_CHAR: char = '.';
-    const RADAR_LINE_RANGE: usize = 1;
-    
-    
+
     let mut parts_store = Vec::new();
     let mut parts_sum: usize = 0;
 
     let mut part_radar: VecDeque<Vec<usize>> = VecDeque::new(); 
     let mut symbol_radar: VecDeque<Vec<Symbol>> = VecDeque::new(); 
-    
+
 
 
     for (line_count, line) in iterate_input().enumerate() {
         debug!(line);
-        
+
         // part radar
         if part_radar.len() > (RADAR_LINE_RANGE + 1) * 2 {
             part_radar.pop_front();
         }
-        
+
         // symbol radar
         if symbol_radar.len() > RADAR_LINE_RANGE * 2 {
-           
+
 
             //debug!("\tsymbol_radar size: {}", symbol_radar.len());
             //debug!("\tpart_radar size: {}", part_radar.len());
 
             if let Some(popped_symbols) = symbol_radar.pop_front() {
 
-                let mut part_ids_in_range: Vec<usize> = Vec::new();
-                for i in 0..(RADAR_LINE_RANGE*2)+1 {
+                let adjacent = get_adjacent_part_ids(
+                    &popped_symbols, &part_radar, &parts_store);
 
-                    if let Some(parts_on_line) = part_radar.get(i) {
-                        //debug!("part_radar index: {}", i);
-                        for part_id in parts_on_line {
-                           
-                            //debug!("- part in range: {}", part.value);
-                            part_ids_in_range.push(*part_id);
-                        }
-                    }
+                for part_id in adjacent {
+                    parts_store[part_id].is_qualified = true;
                 }
-
-                
-               let adjacent_part_ids = find_parts_adjacent_to_symbols(
-                    &popped_symbols, &part_ids_in_range, &parts_store);
-
-                for part_id in adjacent_part_ids {
-                    let ref mut part = parts_store[part_id];
-                    part.is_qualified = true;
-                }
-
             }
 
 
@@ -131,12 +141,12 @@ fn main() {
 
         let mut line_part_radar: Vec<usize> = Vec::new();
         let mut line_symbol_radar: Vec<Symbol> = Vec::new();
-        
+
 
         let mut part_buffer = String::new(); 
 
         for (line_pos, c) in line.chars().enumerate() {
-            
+
             if c.is_numeric() {
                 part_buffer.push(c); 
 
@@ -145,7 +155,7 @@ fn main() {
                 if !part_buffer.is_empty() {
                     // commit part number buffer to store
                     let part_buffer_len = part_buffer.len();
-                    
+
                     debug!("commit part: {}", part_buffer);
                     debug!("\tlen: {}", part_buffer_len);
                     debug!("\tline_pos: {}", line_pos);
@@ -164,7 +174,7 @@ fn main() {
                     part_buffer = String::new();
                 }
 
- 
+
                 if c != PADDING_CHAR {
                     // commit symbol to radar
                     let symbol = Symbol {
@@ -182,13 +192,37 @@ fn main() {
         symbol_radar.push_back(line_symbol_radar);
     }
 
+    debug!("line iteration complete");
+
+    // lines processed; clear up remainder in buffer
+    while symbol_radar.len() > 0 {
+        //debug!("[shift radar]");
+        part_radar.pop_front();
+
+        if let Some(popped_symbols) = symbol_radar.pop_front() {
+
+            let adjacent = get_adjacent_part_ids(
+                &popped_symbols, &part_radar, &parts_store);
+
+            for part_id in adjacent {
+                parts_store[part_id].is_qualified = true;
+            }
+        }
+
+    }
+
+    // sum up results
     for part in parts_store {
         if part.is_qualified {
+            println!("  - qualified: {}", part.value);
+
             let part_value = match part.value.parse() {
                 Ok(v) => v,
                 Err(_) => 0
             };
             parts_sum = parts_sum + part_value;
+        } else {
+            println!("! - qualified: {}", part.value);
         }
     }
 
