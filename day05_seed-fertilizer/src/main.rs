46426384ba5c9d7a714d6fe9fbb79048db74f3b5
Michael Jagroep <michael.jagroep@gmail.com>
2023-12-13 18:10:40 +0000

    impl map parsing

--- day05_seed-fertilizer/src/main.rs
+++ day05_seed-fertilizer/src/main.rs
@@ -1,28 +1,31 @@
-use std::{env, ops::Range};
+use std::{env, ops::Range, collections::HashMap, slice::Iter};
 use crate::input::iterate_input;
 
 mod input;
 struct SeedMapEntry {
     coverage_range: Range<usize>,
-    dest_offset: i32
+    dest_offset: i64
 }
 
 impl SeedMapEntry {
 
-    
+
 
     // parse a map definition line 
     // e.g. 50 98 2
     pub fn parse(definition_line: &str) -> Option<SeedMapEntry> {
         let params: Vec<&str> = definition_line.split(" ").collect();
 
-     
+        if params.len() != 3 {
+            return None;
+        }
+
         let param_dest = params[0].parse::<usize>();
         let param_source = params[1].parse::<usize>();
         let param_length = params[2].parse::<usize>();
 
         if param_dest.is_err() || param_source.is_err() || param_length.is_err() {
-           return None; 
+            return None; 
         }
 
 
@@ -30,15 +33,15 @@ impl SeedMapEntry {
         let param_source = param_source.unwrap();
         let param_length = param_length.unwrap();
 
-        
+
         let entry = Self {
-            dest_offset: param_dest as i32 - param_source as i32,
+            dest_offset: (param_dest as i64) - (param_source as i64),
             coverage_range: param_source..(param_source + param_length - 1)
         };
 
-                
 
-        
+
+
         debug!("parsed: {}", definition_line);
         debug!("\toffset: {}", entry.dest_offset);
         debug!("\trange:");
@@ -47,22 +50,20 @@ impl SeedMapEntry {
 
         return Some(entry);
     }
-    
+
 
 
 }
 
-struct SeedMap<'a> {
-    name: &'a str,
+struct SeedMap {
     entries: Vec<SeedMapEntry>,
 }
 
 
-impl<'a> SeedMap<'a> {
+impl SeedMap {
 
-    pub fn new(name: &'a str) -> Self {
+    pub fn new() -> Self {
         return Self {
-            name,
             entries: Vec::new()
         };
     }
@@ -71,8 +72,12 @@ impl<'a> SeedMap<'a> {
         self.entries.push(entry);
     }
 
+    pub fn iterate_entries(&self) -> Iter<SeedMapEntry> {
+        return self.entries.iter(); 
+    }
+
     pub fn map_value(&self, source_value: usize) -> usize {
-       
+
 
         for map_entry in &self.entries {
             if !map_entry.coverage_range.contains(&source_value){
@@ -87,23 +92,103 @@ impl<'a> SeedMap<'a> {
     }
 }
 
+fn parse_seeds(seed_line: &str) -> Vec<usize> {
+
+    let mut seeds = Vec::new();
+    let seed_line_segments = seed_line.split(" ");
+    for seed_line_segments in seed_line_segments {
+
+        let seed = seed_line_segments.parse::<usize>();
+        if seed.is_ok() {
+            seeds.push(seed.unwrap());
+        }
+    }
+
+    return seeds;
+}
+
 
 fn main() {
-    
+
     env::set_var("PRINT_DEBUG", "true"); 
 
+
+    // parse input into data structures
+    let mut seeds: Vec<usize> = Vec::new();
+    let mut maps: HashMap<String, SeedMap> = HashMap::new();
+
+    let mut is_first = true;
+    let mut current_map_name = String::new();
+    let mut current_map = SeedMap::new();
+
     for line in iterate_input() {
-    //    debug!(line);
+
+        // seed definition
+        if line.starts_with("seeds:") {
+            seeds = parse_seeds(line.as_str());
+            continue;
+        }
+
+        // map definition
+        if line.ends_with("map:") {
+
+            if !is_first {
+                // commit completed map
+                maps.insert(
+                    current_map_name,
+                    current_map);
+
+                current_map = SeedMap::new();
+
+            }
+            current_map_name = line.replace(" map:", "");
+            is_first = false;
+
+            continue;
+        }
+
+        // try parse map entry 
+        if let Some(entry) = SeedMapEntry::parse(line.as_str()) {
+
+            current_map.add_entry(entry);
+        }
     }
-    
-    let mut map = SeedMap::new("test");
-    
-    if let Some(entry) = SeedMapEntry::parse("52 50 48") {
-        map.add_entry(entry);
-    }
-    let dest_value = map.map_value(79);
+    maps.insert(
+        current_map_name,
+        current_map);
+
    
-    debug!("dest value: {}", dest_value);
+    print_parsed_input(&seeds, &maps);
 
 
+    // process input
+    //
+    
+    //
 }
+
+fn print_parsed_input(seeds: &Vec<usize>, maps: &HashMap<String, SeedMap>) {
+    
+    debug!("seeds:");
+    for s in seeds {
+        debug!(s);
+    }
+    
+    for map_key in maps.keys() {
+
+        debug!("{} map:", map_key);
+
+        for seed_map in maps.get(map_key) {
+            for entry in seed_map.iterate_entries() {
+                
+                debug!("\toffset: {}", entry.dest_offset);
+                debug!("\trange:");
+                debug!("\t{}", entry.coverage_range.start);
+                debug!("\t{}", entry.coverage_range.end);
+            }
+        }
+
+    }
+}
+
+
