6880214cf2ec3f4d63f384b8ccf2c75366da49fa
Michael Jagroep <michael.jagroep@gmail.com>
2023-12-16 10:54:50 +0000

    set up day 8

---
+++ day08_haunted-wasteland/src/main.rs
@@ -0,0 +1,16 @@
+use std::env;
+
+mod input;
+
+fn main() {
+
+    env::set_var("PRINT_DEBUG", "true");
+
+    let solution = 0;
+
+
+    debug!("Hello");
+
+
+    print!("Solution: {}", solution);
+}
