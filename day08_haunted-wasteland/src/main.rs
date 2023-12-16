288be98f55eb717bae75538ce4234e87b9f4d3c5
Michael Jagroep <michael.jagroep@gmail.com>
2023-12-16 13:28:51 +0000

    part 2 solution wip; needs optimisation

--- day08_haunted-wasteland/src/main.rs
+++ day08_haunted-wasteland/src/main.rs
@@ -23,7 +23,7 @@ fn main() {
 
     let mut directions = Vec::new();
     let mut nodes = HashMap::new();
-
+    let mut starting_nodes = Vec::new();
 
     for (ix, line) in iterate_input().enumerate() {
 
@@ -33,14 +33,87 @@ fn main() {
         }
 
         if let Some(node) = parse_node(line)  {
-            nodes.insert(node.label.to_string(), node);
+
+            let node_label = node.label.clone();
+
+            nodes.insert(node.label.clone(), node);
+
+            if node_label.ends_with('A') {
+                starting_nodes.push(node_label);
+            }
         }
     }
     
     print_parsed_data(&directions, &nodes);
 
 
-    // walk 
+    //solution = walk_map_part_one(&directions, &nodes);
+
+    //print!("Solution part 1: {}", solution);
+
+    solution = walk_map_part_two(&starting_nodes, &directions, &nodes);
+
+    print!("Solution part 2: {}", solution);
+
+
+}
+
+fn walk_map_part_two(
+    starting_nodes: &Vec<String>, 
+    directions: &Vec<bool>, 
+    nodes: &HashMap<String, MapNode>) -> usize {
+    
+    let mut steps = 0;
+    let mut current_nodes = Vec::new();
+    let node_count = starting_nodes.len();
+    for n in starting_nodes {
+        current_nodes.push(n.to_string());
+    }
+
+    loop {
+        
+        let mut is_at_destination = true;
+
+        for ix in 0..node_count {
+
+            let current_node = match current_nodes.get(ix) {
+                Some(v) => v,
+                None => { panic!("Invalid node index '{}'", ix) }
+            };
+
+            let node_entry = match nodes.get(current_node) {
+                Some(v) => v,
+                None => { panic!("Node '{}' isn't on the map", current_node)}
+            };
+
+            let direction_ix = steps % directions.len();
+            let direction = directions[direction_ix];
+
+            if direction == NODE_DIRECTION_LEFT {
+                current_nodes[ix] = node_entry.left.clone();
+            } else if direction == NODE_DIRECTION_RIGHT {
+                current_nodes[ix] = node_entry.right.clone();
+            } else {
+                panic!("Invalid direction at step {}", steps);
+            }
+
+            is_at_destination &= current_nodes[ix].ends_with('Z');
+            
+        }
+        
+        steps += 1;
+
+        // check if at destination
+        if is_at_destination {
+            break;
+        }
+
+    }
+    return steps;
+}
+
+fn walk_map_part_one(directions: &Vec<bool>, nodes: &HashMap<String, MapNode>) -> usize {
+    
     let mut current_node = "AAA";
     let mut steps = 0;
     while current_node != "ZZZ" {
@@ -63,11 +136,9 @@ fn main() {
         
         steps += 1;
     }
-
-    solution = steps;
-
-    print!("Solution: {}", solution);
+    return steps;
 }
+
 
 fn print_parsed_data(directions: &Vec<bool>, nodes: &HashMap<String, MapNode>) {
     
