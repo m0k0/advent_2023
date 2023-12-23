5057807d8f121f6f010b217e5b4a0eb0fe3ad019
Michael Jagroep <michael.jagroep@gmail.com>
2023-12-23 20:59:41 +0000

    part 1 solution

--- day09_mirage-maintenance/src/main.rs
+++ day09_mirage-maintenance/src/main.rs
@@ -7,6 +7,8 @@ mod input;
 fn main() {
 
     env::set_var("PRINT_DEBUG", "true");
+ 
+    let mut solution = 0;
 
     for (line_ix, line) in iterate_input().enumerate() {
 
@@ -20,31 +22,51 @@ fn main() {
 
             line_sequence.push(parsed_segment);
         }
+
+        let next_value = calc_next_value(&line_sequence);
+        solution += next_value; 
+        debug!(line);
+        debug!("next: {}", next_value);
     }
+
+    println!("Solution: {}", solution);
 }
 
 
-fn calc_reduced(sequence: Vec<isize>) {
+fn calc_next_value(sequence: &Vec<isize>) -> isize {
     
-    let mut reduced_sequence = calc_reduced_sequence(sequence);
+    let reduced_sequence = calc_reduced_sequence(sequence);
     
-    let mut isAllZeroes = true;
+    let mut is_all_zero = true;
     for num in reduced_sequence.iter() {
         
         if *num != 0 {
-            isAllZeroes = false;
+            is_all_zero = false;
             break;
         }
     }
 
-    if isAllZeroes {
-        // done
+    let last_value = match sequence.last() {
+        Some(v) => *v,
+        None => 0
+    };
+
+    let mut last_value_reduced = match reduced_sequence.last() {
+        Some(v) => *v,
+        None => 0
+    };
+
+    if !is_all_zero {
+        last_value_reduced = calc_next_value(&reduced_sequence);
     }
 
+    let next_value = last_value + last_value_reduced;
+
+    return next_value;
 
 }
 
-fn calc_reduced_sequence(sequence: Vec<isize>) -> Vec<isize> {
+fn calc_reduced_sequence(sequence: &Vec<isize>) -> Vec<isize> {
 
     let mut reduced_sequence = Vec::new();
 
