0733067035b6787ce8eae133e7dcce5d01ecaa9a
Michael Jagroep <michael.jagroep@gmail.com>
2024-01-04 16:27:17 +0000

    day 11 wip

--- day11_cosmic-expansion/src/main.rs
+++ day11_cosmic-expansion/src/main.rs
@@ -1,16 +1,48 @@
 use std::env;
 
+use grid::Matrix;
 use input::input_as_grid;
 
 mod input;
 mod grid;
 
+const CHAR__EMPTY_SPACE: char = '.';
+const CHAR__GALAXY: char = '#';
+
+
 fn main() {
     
     env::set_var("PRINT_DEBUG", "TRUE");
 
-    let grid = input_as_grid();
+    let mut grid = input_as_grid();
 
-    grid.print_grid()
+    // input grid
+    grid.print_grid();
+
 }
 
+fn expand_space(space_grid: Matrix<char>) -> Matrix<char>{ 
+
+    let mut expanded_space = Matrix::new();
+
+    for y in 0..space_grid.height()  {
+        
+        let mut is_empty_space = true;
+        
+        let row  = space_grid.get_row_values(y);
+ 
+        for cell in row {
+        
+            if !cell.is_some_and(|x| *x == CHAR__EMPTY_SPACE) {
+                is_empty_space = false;
+                break;
+            }
+        }
+
+        expanded_space.insert_row(y, row);
+        
+    }
+
+    return expand_space(expanded_space);
+
+}
